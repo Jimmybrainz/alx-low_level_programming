a8777b412876085b47e58669e849311886f4b24b
Jimmybrainz <jim44mike@gmail.com>
2023-09-21 19:07:49 +0000

    function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,13 +7,20 @@
 
 void print_number(int n)
 {
+	unsigned int num;
+
+	/* check if number is negative */
 	if (n < 0)
 	{
+		num = -n;
 		_putchar('-'); /*print minus sign for negative numbers */
-		n = -n; /* make n positive */
+	}
+	else
+	{
+		num = n;
 	}
 
-	if (n / 10 != 0)
+	if (num / 10)
 	{
 		print_number(n / 10); /* Recursively print the remaining digits */
 	}
