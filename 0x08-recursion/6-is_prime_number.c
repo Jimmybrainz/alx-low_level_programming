0e6a2a94a195f4aaa2e593aba7b9a50a27f64aac
Jimmybrainz <jim44mike@gmail.com>
2023-09-28 21:54:32 +0000

    function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,34 +1,51 @@
 #include "main.h"
 
+int isDivisible(int num, int divisor);
+
 /**
- * is_prime_number - function to check if an integer is a prime number
- * @n: number to check
- * Return: 1 if n is a prime number, otherwise return 0
+ * is_prime_number - function that returns 1 if the input integer
+ * is a prime number, otherwise return 0
+ * @n: input number to check if if it's prime
+ * Return: 1 if prime number, 0 if otherwise
  */
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	if (n < 2) /* numbers less than two are not prime numbers */
+	{
 		return (0);
-
-	return (check_divisor(n, n - 1));
+	}
+	else if (n <= 3) /* 2 and 3 are prime numbers */
+	{
+		return (1);
+	}
+	else
+	{
+		return (isDivisible(n, 2));/* for numbers greater than 2, check divisibity */
+									/* starting from 2 */
+	}
 }
 
-
 /**
- * check_divisor - helper function to check if an integer is a prime number
- * @n: number to check
- * @m: divisor
- * Return: 1 if n is a prime number, otherwise return 0
+ * isDivisible - function to check for prime by checking the
+ * it divisibiltity
+ * @num: number to check if it's prime
+ * @divisor: divisor to check number divisibilty
+ * Return: 1 if prime, 0 if not.
  */
 
-int check_divisor(int n, int m)
+int isDivisible(int num, int divisor)
 {
-	if (m <= 1)
-		return (1);
-
-	if (n % m == 0)
-		return (0);
-
-	return (check_divisor(n, m - 1));
+	if (num % divisor == 0)
+	{
+		return (0); /* is not a prime */
+	}
+	else if (divisor == num / 2)
+	{
+		return (1); /* it's a prime number */
+	}
+	else
+	{
+		return (isDivisible(num, divisor + 1));
+	}
 }
