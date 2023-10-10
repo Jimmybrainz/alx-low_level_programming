2a5a52713f9da27167c371f8b3a5257dc850acc9
Jimmybrainz <jim44mike@gmail.com>
2023-10-10 11:39:31 +0000

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *struct dog - struct containing dog owner informations
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
