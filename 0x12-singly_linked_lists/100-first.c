2133d7d13fef180c1c4e732f2421d284d1140413
Jimmybrainz <jim44mike@gmail.com>
2023-10-19 09:47:40 +0000

    function that prints before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#inlcude "lists.h"
+
+/**
+ *print_before_main - This function prints a specific message before the
+ *main function is executed
+ *Description: Attributes; __attribute__((constructor)) makes this function
+ *run before main
+ *Return: (void)
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
